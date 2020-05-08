7ec1beb63b6b887c3267bb8844f318581a472b53
Oleksiy Perkin <perkin.oleksiy@gmail.com>
2020-05-08 15:57:23 +0300

    Customized portfol routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :portfols
+  resources :portfols, exept: [:show]
+  get 'portfol/:id', to: 'portfols#show', as: 'portfol_show'
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'  
