6012efa21bf950e3fc1ebe59b0b970013a176416
Oleksiy Perkin <perkin.oleksiy@gmail.com>
2020-05-08 12:06:59 +0300

    Integrated new and create actions for portfols

--- app/controllers/portfols_controller.rb
+++ app/controllers/portfols_controller.rb
@@ -2,4 +2,21 @@ class PortfolsController < ApplicationController
   def index
     @portfolio_items = Portfol.all
   end
+
+  def new 
+    @portfolio_item = Portfol.new
+  end
+
+  def create
+    @portfolio_item = Portfol.new(params.require(:portfol).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfols_path, notice: 'Yout portfolio item is now live!' }
+      else
+        format.html { render :new }        
+      end
+    end
+  end
+
 end
