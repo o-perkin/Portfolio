f8123fe9451480439f48967d0e709ed69d79c354
Oleksiy Perkin <perkin.oleksiy@gmail.com>
2020-06-03 16:05:57 +0300

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,22 @@
+User.create!(
+  email: "alex@gmail.com",
+  password: "asdasd",
+  password_confirmation: 'asdasd',
+  name: 'Admin User',
+  roles: "site_admin"
+)
+
+puts "1 admin user created"
+
+User.create!(
+  email: "lol@gmail.com",
+  password: "asdasd",
+  password_confirmation: 'asdasd',
+  name: 'regular User'
+)
+
+puts "1 regular user created"
+
 3.times do |topic|
   Topic.create!(
     title: "Topic #{topic}"
